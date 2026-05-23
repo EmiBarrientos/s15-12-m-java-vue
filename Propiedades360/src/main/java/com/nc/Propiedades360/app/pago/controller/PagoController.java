683f84f19c5c347aff96216616cbc2f32d395a33
emiliano <emilianogbarrientos@gmail.com>
2026-05-22 21:04:23 -0300

    actualizando endpoints en controllers

--- Propiedades360/src/main/java/com/nc/Propiedades360/app/pago/controller/PagoController.java
+++ Propiedades360/src/main/java/com/nc/Propiedades360/app/pago/controller/PagoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/pagos")
+@RequestMapping("/api/pagos")
 public class PagoController {
 
 
--- Propiedades360/src/main/java/com/nc/Propiedades360/app/visita/controller/VisitaController.java
+++ Propiedades360/src/main/java/com/nc/Propiedades360/app/visita/controller/VisitaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/visitas")
+@RequestMapping("/api/visitas")
 public class VisitaController {
 
     private final VisitaService visitaService;
